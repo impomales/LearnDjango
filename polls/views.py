57d84f5dc7f759289da222ac0aa7f785d9cd862e
Isaias <impomales@gmail.com>
2016-06-08 23:04:57 +0000

    add index template

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Question
 
@@ -6,8 +7,10 @@ from .models import Question
 # Displays latest questions.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context);
     
 # displays question text
 def detail(request, question_id):
