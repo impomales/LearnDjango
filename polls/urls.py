096d6f2cf44d3224e032da0c223be7356a439a86
Isaias <impomales@gmail.com>
2016-06-09 00:13:38 +0000

    add detail template and url namespacing

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+# add namespace to differentiate among apps within project.
+app_name = 'polls'
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
